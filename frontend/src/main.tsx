fd6b1983feeb2d28c31ecafae1ba6d3dd374cfd8
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-21 14:37:52 -0500

    frontend and end to end tests

---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error(
+    "Root element not found. Make sure there's an element with id='root' in your HTML."
+  )
+}
+
+ReactDOM.createRoot(rootElement).render(<App />)
